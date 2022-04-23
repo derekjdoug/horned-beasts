fe1ba7c1d2cedc9d6954db87bf1b37e66858e479
Derek Douglas <derek.j.douglas13@gmail.com>
2022-04-23 17:30:26 -0500

    finished lab, edited pictures and added them to public folder

--- src/Main.js
+++ src/Main.js
@@ -5,9 +5,9 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <HornedBeast title={'Narwhal'} imgUrl={'https://www.euclidlibrary.org/sites/default/files/tickles/narwhal%20under%20the%20sea.jpg'} description={'whale unicorn'} />
-        <HornedBeast title={'Ibex'} imgUrl={'https://animals.sandiegozoo.org/sites/default/files/inline-images/nubian_ibex_male.jpg'} description={'dramatic goat'} />
-        <HornedBeast title={'Rhinoceros'} imgUrl={'https://i.natgeofe.com/k/0e9fa05c-7cdb-4e15-a32a-b4fd60d64709/black-rhino-closeup.jpg'} description={'endangered species'} />
+        <HornedBeast title={'Narwhal'} imgUrl={'img/narwhal.jpg'} description={'whale unicorn'} />
+        <HornedBeast title={'Ibex'} imgUrl={'img/ibex.jpg'} description={'dramatic goat'} />
+        <HornedBeast title={'Rhinoceros'} imgUrl={'img/rhino.jpg'} description={'endangered species'} />
       </>
     )
   }
