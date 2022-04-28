5f113fcdcd105f6353da9bfadaab2fbf7a61cc32
Derek Douglas <derek.j.douglas13@gmail.com>
2022-04-27 20:53:06 -0500

    Finished basic reqs for lab02

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Number and name of feature: ________________________________
 
 Estimate of time needed to complete: _____
 
-Start time: _____
+Start time: 0737____
 
 Finish time: _____
 
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    // eslint-disable-next-line no-lone-blocks
+    super(props);
+    this.state = {
+      clicks: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState({clicks: this.state.clicks + 1});
+  }
+
   render() {
     return (
       <>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imgUrl} alt={this.props.description} title={this.props.title} />
+        <Image src={this.props.imgUrl} alt={this.props.description} title={this.props.title} onClick={this.handleClick} roundedCircle={true} className="beastImg" />
         <p>{this.props.description}</p>
+        <p>❤️ {this.state.clicks}</p>
       </>
     )
   }
