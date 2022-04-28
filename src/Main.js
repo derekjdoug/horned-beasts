cc27f28064cd8060008981ba95201d40b0f76142
Derek Douglas <derek.j.douglas13@gmail.com>
2022-04-27 22:51:31 -0500

    added remaining photos

--- src/Main.js
+++ src/Main.js
@@ -15,6 +15,17 @@ class Main extends React.Component {
         <HornedBeast title={'Baby Rhino'} imgUrl={this.props.imageURLs[6].image_url} description={'This is actually a figurine but it looks kinda real'} clicks={0} />
         <HornedBeast title={'Cera'} imgUrl={this.props.imageURLs[7].image_url} description={'Three horns but still, horns. And who doesn\'t like The Land Before Time?'} clicks={0} />
         <HornedBeast title={'Narwhal costume'} imgUrl={this.props.imageURLs[8].image_url} description={'A woman wearing a blue narwhal costume'} clicks={0} />
+        <HornedBeast title={'Rhino costume'} imgUrl={this.props.imageURLs[9].image_url} description={'Mascots have to get their costumes somewhere'} clicks={0} />
+        <HornedBeast title={'Believe'} imgUrl={this.props.imageURLs[10].image_url} description={'I believe in unicorns, do you?'} clicks={0} />
+        <HornedBeast title={'Markhor'} imgUrl={this.props.imageURLs[11].image_url} description={'These wild goats eat snakes, then secrete a foam that locals fight over for the antivemon properties -- how cool is that?'} clicks={0} />
+        <HornedBeast title={'Baby markhor'} imgUrl={this.props.imageURLs[12].image_url} description={'Even the babies are adorable'} clicks={0} />
+        <HornedBeast title={'Mouflon'} imgUrl={this.props.imageURLs[13].image_url} description={'Those horns though'} clicks={0} />
+        <HornedBeast title={'Addax'} imgUrl={this.props.imageURLs[14].image_url} description={'This guy is basically extinct but survives well in captivity, so they\'re frequently found in zoos'} clicks={0} />
+        <HornedBeast title={'Baby mouflon'} imgUrl={this.props.imageURLs[15].image_url} description={'The cuteness that is a baby mouflon asleep'} clicks={0} />
+        <HornedBeast title={'Happy Jackson\'s Chameleon'} imgUrl={this.props.imageURLs[16].image_url} description={'These are really common in Hawaii'} clicks={0} />
+        <HornedBeast title={'Serious Jackson\'s Chameleon'} imgUrl={this.props.imageURLs[17].image_url} description={'This one is very serious.'} clicks={0} />
+        <HornedBeast title={'Horned Lizard'} imgUrl={this.props.imageURLs[17].image_url} description={'Fave food: ants'} clicks={0} />
+        <HornedBeast title={'Smaug'} imgUrl={this.props.imageURLs[17].image_url} description={'Fan illustration of Smaug from The Hobbit'} clicks={0} />
       </>
     )
   }
