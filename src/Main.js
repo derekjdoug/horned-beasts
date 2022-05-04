481ac692e57a2006b0c9ac09224ea6cbaa121549
Derek Douglas <derek.j.douglas13@gmail.com>
2022-05-03 23:05:21 -0500

    finished initial run through of lab04

--- src/Main.js
+++ src/Main.js
@@ -7,12 +7,13 @@ class Main extends React.Component {
     return (
       <>
         <Row xs={1} sm={2} md={3} lg={4}>
-          {this.props.imageURLs.map(beast => (
+          {this.props.allBeasts.map(beast => (
             <HornedBeast
               key={beast._id}
               title={beast.title}
               imgUrl={beast.image_url}
               description={beast.description}
+              horns={beast.horns}
               handleShowModal={this.props.handleShowModal}
             />
           ))}
